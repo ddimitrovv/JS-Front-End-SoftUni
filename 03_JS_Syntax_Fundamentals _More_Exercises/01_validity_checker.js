c3bbd4fe99cc12d70ccffc6eb25d1c57d47ad2df
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-02-22 19:58:52 +0200

    added new tasks

--- 03_JS_Syntax_Fundamentals _More_Exercises/01_validity_checker.js
+++ 03_JS_Syntax_Fundamentals _More_Exercises/01_validity_checker.js
@@ -1,24 +1,24 @@
 function validityChecker(x1, y1, x2, y2) {
 
-    let firstNum = Math.sqrt(((0 - y1) ** 2) + ((0 - y1) ** 2));
-    let secondNum = Math.sqrt(((x2 - 0) ** 2) + ((y2 - 0) ** 2));
-    let thirdNum = Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2));
+    let firstNum = Number.isInteger(Math.sqrt(((0 - x1) ** 2) + ((0 - y1) ** 2)));
+    let secondNum = Number.isInteger(Math.sqrt(((x2 - 0) ** 2) + ((y2 - 0) ** 2)));
+    let thirdNum = Number.isInteger(Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2)));
     
-    if (Number.isInteger(firstNum)) {
-        console.log(`{${x1}, ${y1}} to {${0}, ${0}} is valid`)
+    if (firstNum) {
+        console.log(`{${x1}, ${y1}} to {${0}, ${0}} is valid`);
     } else {
-        console.log(`{${x1}, ${y1}} to {${0}, ${0}} is invalid`)
+        console.log(`{${x1}, ${y1}} to {${0}, ${0}} is invalid`);
     }
 
-    if (Number.isInteger(secondNum)) {
-        console.log(`{${x2}, ${y2}} to {${0}, ${0}} is valid`)
+    if (secondNum) {
+        console.log(`{${x2}, ${y2}} to {${0}, ${0}} is valid`);
     } else {
-        console.log(`{${x2}, ${y2}} to {${0}, ${0}} is invalid`)
+        console.log(`{${x2}, ${y2}} to {${0}, ${0}} is invalid`);
     }
 
-    if (Number.isInteger(thirdNum)) {
-        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`)
+    if (thirdNum) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
     } else {
-        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`)
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
     }
 }
