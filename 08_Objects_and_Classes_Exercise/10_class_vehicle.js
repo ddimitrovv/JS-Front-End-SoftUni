7a65af090c019c073a8d36fedbe824657edba715
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-02-28 11:13:24 +0200

    added new tasks

--- 08_Objects_and_Classes_Exercise/10_class_vehicle.js
+++ 08_Objects_and_Classes_Exercise/10_class_vehicle.js
@@ -11,13 +11,3 @@ class Vehicle {
         return this.fuel -= fuelLoss
     }
 }
-
-// let parts = { engine: 6, power: 100 };
-// let vehicle = new Vehicle('a', 'b', parts, 200);
-// vehicle.drive(100);
-// console.log(vehicle.fuel);
-// console.log(vehicle.parts.quality);
-let parts = {engine: 9, power: 500};
-let vehicle = new Vehicle('l', 'k', parts, 840);
-vehicle.drive(20);
-console.log(vehicle.fuel);
