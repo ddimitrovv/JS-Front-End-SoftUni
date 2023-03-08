d5aad8992f44d0be3c2c2dde5d5846b47dec602d
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-03-08 10:01:16 +0200

    added changes

--- 05_Functions_and_Statements_Exercises/06_password_validator.js
+++ 05_Functions_and_Statements_Exercises/06_password_validator.js
@@ -20,3 +20,35 @@ function passwordValidator(password) {
         console.log('Password is valid');
     }
 }
+
+// function passwordValidator(password) {
+
+//     let condition = true;
+    
+//     if (lengthValidation(password)) {
+//         condition = false;
+//         console.log('Password must be between 6 and 10 characters');
+//     }
+//     if (letterNumber(password)) {
+//         condition = false;
+//         console.log('Password must consist only of letters and digits');
+//     }
+//     if (!passwordMatch(password)) {
+//         condition = false;
+//         console.log('Password must have at least 2 digits');
+//     }
+//     if (condition) {
+//         console.log('Password is valid');
+//     }
+
+//     function lengthValidation(pass) {
+//         return password.length < 6 || 10 < password.length
+//     }
+//     function passwordMatch(pass) {
+//          return (password.match(/\d/g) || []).length >= 2;
+//     }
+//     function letterNumber(pass) {
+//         let letterNumber = /^\w+$/;
+//         return !pass.match(letterNumber)
+//     }
+// }
