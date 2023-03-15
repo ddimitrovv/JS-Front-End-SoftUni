bb74a8734a17c5ef5a54f7eb0ad6d62d7a7d73e0
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-03-15 11:40:23 +0200

    added new tasks

--- 10_DOM_and_events_lab/06. Add-Delete/app.js
+++ 10_DOM_and_events_lab/06. Add-Delete/app.js
@@ -1,11 +1,11 @@
 function addItem() {
-    const textToAdd = document.getElementById('newItemText').value
+    const textToAdd = document.getElementById('newItemText').value;
     console.log(textToAdd);
     let anchor = document.createElement('a');
     anchor.href = '#';
     let li = document.createElement('li');
     anchor.appendChild(document.createTextNode('[Delete]'));
-    anchor.addEventListener('click', deleteItem)
+    anchor.addEventListener('click', deleteItem);
     li.textContent = textToAdd;
     li.appendChild(anchor);
     document.getElementById('items').appendChild(li);
