152d4b77cfcc4fc2243b5d12ba273793e4c4cdc4
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-03-23 15:24:45 +0200

    added new tasks

---
+++ JS-Front-End_Exams/01_exam_preparation_I/02_scary_story/app.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", solve);
+
+function solve() {
+  const publishButton = document.getElementById('form-btn')
+  publishButton.addEventListener('click', () => {
+    const firstName = document.getElementById('first-name');
+    const lastName = document.getElementById('last-name');
+    const age = document.getElementById('age');
+    const storyTitle = document.getElementById('story-title');
+    const story = document.getElementById('story');
+  })
+}
