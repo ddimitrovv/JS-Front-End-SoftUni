4c81cb51359da527de2c2a6a950b851c071d0b54
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-03-24 10:13:21 +0200

    added new tasks

--- JS-Front-End_Exams/01_exam_preparation_I/02_scary_story/app.js
+++ JS-Front-End_Exams/01_exam_preparation_I/02_scary_story/app.js
@@ -8,9 +8,16 @@ function solve() {
     const storyTitle = document.getElementById('story-title');
     const genre = document.getElementById('genre');
     const story = document.getElementById('story');
+    let currentFirstName = null
+    let currentLastName = null;
+    let currentAge = null;
+    let currentStoryTitle = null;
+    let currentGenre = null;
+    let currentStory = null;
 
-    publishButton.addEventListener('click', () => {
-        
+    publishButton.addEventListener('click', publish);
+
+    function publish() {
         if (firstName && lastName && age.value && storyTitle && story.value) {
             const ul = document.getElementById('preview-list')
             let li = document.createElement('li');
@@ -18,6 +25,13 @@ function solve() {
             let article = document.createElement('article');
             let heading = document.createElement('h4');
             heading.textContent = `Name: ${firstName.value} ${lastName.value}`;
+            currentFirstName = firstName.value;
+            currentLastName = lastName.value;
+            currentAge = age.value;
+            currentStoryTitle = storyTitle.value;
+            currentGenre = genre.value;
+            currentStory = story.value;
+            
             firstName.value = '';
             lastName.value = '';
             let firstP = document.createElement('p');
@@ -40,13 +54,22 @@ function solve() {
                 let heading = document.createElement('h1');
                 heading.textContent = 'Your scary story is saved!';
                 divMain.appendChild(heading);
-            })
+            });
             let editButton = document.createElement('button');
             editButton.textContent = 'Edit Story';
             editButton.className = 'edit-btn';
             editButton.addEventListener('click', () => {
+                firstName.value = currentFirstName;
+                lastName.value = currentLastName;
+                age.value = currentAge;
+                storyTitle.value = currentStoryTitle;
+                genre.value = currentGenre;
+                story.value = currentStory;
+                editInfo = Array.from(document.getElementsByClassName('story-info'))[0];
+                editInfo.remove();
+                publishButton.disabled = false;
                 
-            })
+            });
             let deleteButton = document.createElement('button');
             deleteButton.textContent = 'Delete Story';
             deleteButton.className = 'delete-btn';
@@ -54,12 +77,12 @@ function solve() {
                 let target = document.querySelector('#preview-list li');
                 target.remove();
                 publishButton.disabled = '';
-            })
+            });
 
             article.append(heading, firstP, secondP, thirdP, fourthP);
             li.append(article, saveButton, editButton, deleteButton);
             ul.append(li);
             publishButton.disabled = 'disabled';
         }
-  });
+    }
 }
